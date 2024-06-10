a2433ee358c9044b09969b7e88a4785862f4b4c9
ArtemKarDev <akm86@mail.ru>
2024-06-10 21:59:32 +0300

    added test addToCart

--- src/test/java/tests/demowebshop/CartTests.java
+++ src/test/java/tests/demowebshop/CartTests.java
@@ -7,8 +7,8 @@ import static org.hamcrest.Matchers.is;
 
 public class CartTests extends TestBase {
     @Test
-    void addToCartTest(){
-
+    void addToCartAsAuthTest(){
+//move to api class
         String authCookieKey = "NOPCOMMERCE.AUTH";
         String authCookieValue = given()
                 // .headers("Content-type","application/x-www-form-urlencoded")
@@ -22,6 +22,8 @@ public class CartTests extends TestBase {
                 .statusCode(302)
                 .extract()
                 .cookie(authCookieKey);
+
+//get actual cart size
 
         String data = "product_attribute_72_5_18=53"+
                 "&product_attribute_72_6_19=54"+
@@ -39,6 +41,27 @@ public class CartTests extends TestBase {
                 .body("success", is(true))
                 .body("message", is("The product has been added to your \\u003ca href=\\\"/cart\\\"\\u003eshopping cart\\u003c/a\\u003e"));
 
+        // todo check cart size
+    }
+
+    @Test
+    void addToCartUnAuthTest(){
+
+        String data = "product_attribute_72_5_18=53"+
+                "&product_attribute_72_6_19=54"+
+                "&product_attribute_72_3_20=57"+
+                "&addtocart_72.EnteredQuantity=1";
+
+        given()
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .body(data)
+                .when()
+                .post("/addproducttocart/details/72/1")
+                .then()
+                .statusCode(200)
+                .body("success", is(true))
+                .body("message", is("The product has been added to your \\u003ca href=\\\"/cart\\\"\\u003eshopping cart\\u003c/a\\u003e"))
+                .body("updatetopcartsectionhtml", is("(2)"));
     }
 
 }
