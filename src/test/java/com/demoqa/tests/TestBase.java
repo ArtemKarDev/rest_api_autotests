4e51d91bf30fb1718a043de662642477fbc4ea33
ArtemKarDev <akm86@mail.ru>
2024-06-12 16:59:19 +0300

    added test login api demoqa

---
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -0,0 +1,14 @@
+package com.demoqa.tests;
+
+import com.codeborne.selenide.Configuration;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.baseUrl = "https://demoqa.com";
+        RestAssured.baseURI = "https://demoqa.com";
+    }
+}
---
+++ src/test/java/com/demoqa/tests/TestData.java
@@ -0,0 +1,6 @@
+package com.demoqa.tests;
+
+public class TestData {
+    public static String login = "test123456",
+                        password = "Test123456@";
+}
